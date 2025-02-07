0f48922d994c5d2232059e4efbbae3a5abc6a7ed
addybansal <adarshkumar88070@gmail.com>
2025-02-07 12:24:30 +0530

    update 07-02-2025

--- Desktop/abes/CS-C/Backend/file1.js
+++ Desktop/abes/CS-C/Backend/file1.js
@@ -1,4 +1,14 @@
-const fs = require("fs");
+ const fs = require("fs");
 const data = fs.readFileSync("./data.txt","utf8");
-console.log(data)
+console.log(data);
+if(data.match("H")){
+    console.log("H is present")
+    const newdata = data.replace("H","AB")
+    fs.writeFileSync("./data.txt",newdata,"utf-8");
+}
 
+// fs.writeFileSync("./data1.txt","hello,this is my new work","utf-8");
+// fs.writeFileSync("./data1.txt","abes" , "utf-8");
+// fs.appendFileSync("./data1.txt","engineering college","utf-8");
+
+// fs.renameSync("./data2.txt","./data3.txt");
